e83a0ece1e0f6fe75b5a147a15a51ef64627eb64
Ramon Henrique Pereira Ribeiro <rhpr@ug1c44.windows.cin.ufpe.br>
2016-10-05 07:47:32 -0300

    Ajeitando o arquivo .dat e o .sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ mkdir build
 cd build
 cmake ..
 cmake --build . --config Release
-wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
-bunzip2 shape_predictor_68_face_landmarks.dat.bz2
+#wget http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+#bunzip2 shape_predictor_68_face_landmarks.dat.bz2
