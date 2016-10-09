4b8e7c41122aecc97fc7993871e2cc1f1b76485d
Ramon Henrique <rhpr@cin.ufpe.br>
2016-10-09 09:10:16 -0300

    Dando pull na branch de pumba

--- face_landmark_detection_ex.cpp
+++ face_landmark_detection_ex.cpp
@@ -43,7 +43,10 @@
 #include <dlib/image_processing.h>
 #include <dlib/gui_widgets.h>
 #include <dlib/image_io.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <math.h>
 
 using namespace dlib;
 using namespace std;
@@ -51,24 +54,129 @@ using namespace std;
 // ----------------------------------------------------------------------------------------
 
 
-void extrairCaracteristicas(full_object_detection shape, float* landmarks)
+
+float media(float* array, int tamanho)
+{
+	float resultado = 0;
+	for(int i = 0; i < tamanho; i++)
+	{
+		resultado += array[i];
+	}
+
+	resultado /= tamanho;
+
+	return resultado;
+}
+
+void desenharPontos(full_object_detection shape, array2d<rgb_pixel> *imagem, point gravidade)
+{	
+	if(gravidade.x() > 0 && gravidade.y() > 0)
+	{
+		
+		for(int i = 0; i < shape.num_parts(); i++)
+		{
+			//desenha landmarks
+			draw_solid_circle (*imagem, shape.part(i),4, rgb_pixel(0,255,0));
+			//desenha linhas entre o ponto de gravidade e os landmarks
+			draw_line(*imagem, gravidade, shape.part(i), rgb_pixel(255,0,0));
+		}
+		//desenha ponto de gravidade
+		draw_solid_circle (*imagem, gravidade,4, rgb_pixel(0,0,255));
+	}
+}
+
+void extrairCaracteristicas(full_object_detection shape, float* landmarks, point* pontoDeGravidade, float* landmarks_vetorizada)
 {
 	int cont = 0;
+	float anguloNariz;
+	float landmarksX[68];
+	float landmarksY[68];
+	float distancia_x_Centro[68];
+	float distancia_y_Centro[68];
+
+	
+
+	float xMax, xMin, yMax, yMin;
 
 	for(int i = 0; i < shape.num_parts(); i++)
 	{
+		landmarksX[i] = (float)shape.part(i).x();
 		landmarks[cont] = (float)shape.part(i).x();
 		cont++;
+		landmarksY[i] = (float)shape.part(i).y();
 		landmarks[cont] = (float)shape.part(i).y();
 		cont++;
 	}
+
+	*pontoDeGravidade = point(media(landmarksX, 68), media(landmarksY, 68));
+
+	for(int i = 0; i < shape.num_parts(); i++)
+	{
+		distancia_x_Centro[i] = landmarksX[i] - pontoDeGravidade->x();
+		distancia_y_Centro[i] = landmarksY[i] - pontoDeGravidade->y();
+	}
+
+	if(landmarksX[26] == landmarksX[29])
+	{
+		anguloNariz = 0;
+	}
+	else
+	{
+		anguloNariz = atan((landmarksY[26]-landmarksY[29])/(landmarksX[26]-landmarksX[29]))*180/M_PI;
+	}
+
+	if(anguloNariz < 0)
+	{
+		anguloNariz += 90;
+	}
+    else
+    {
+    	anguloNariz -= 90;
+    }
+
+    cont = 0;
+    for(int i = 0; i < shape.num_parts(); i++)
+    {
+    	//distancia do ponto x ao centro
+    	landmarks_vetorizada[cont] = distancia_x_Centro[i];
+    	cont++;
+    	//distancia do ponto y ao centro
+    	landmarks_vetorizada[cont] = distancia_y_Centro[i];
+    	cont++;
+    	//norma do vetor
+    	landmarks_vetorizada[cont] = sqrt(pow((landmarksX[i] - pontoDeGravidade->x()),2) + pow((landmarksY[i] - pontoDeGravidade->y()),2));
+    	cont++;
+    	//angulo relativo
+    	landmarks_vetorizada[cont] = (atan((landmarksY[i]-pontoDeGravidade->y())/(landmarksX[i]-pontoDeGravidade->x()))*180/M_PI) - anguloNariz;
+    	cont++;
+    }
+
+    cout << " landmarks vetorizada: " << endl;
+    cont = 1;
+    
+    for(int i = 0; i < 272; i++)
+    {
+    	if(i%4 == 0)
+    	{ 
+    		cout << endl << "Parte " << cont << ": " << endl;
+    		cont++;
+    	}
+
+    	cout << landmarks_vetorizada[i] << " ";
+    	
+    	
+
+    }
 }
 
 
 
 int main(int argc, char** argv)
-{  
+{
+	float landmarks_vetorizada[272];  
 	float landmarks[136];
+	point pontoDeGravidade = point(0,0);
+	float mediaX = 0, mediaY = 0;
     try
     {
         // This example takes in a shape model file and then a list of images to
@@ -118,14 +226,20 @@ int main(int argc, char** argv)
                 full_object_detection shape = sp(img, dets[j]);
               
               	if(shape.num_parts() > 0)
-        			extrairCaracteristicas(shape, landmarks);
+              	{
+        			extrairCaracteristicas(shape, landmarks, &pontoDeGravidade, landmarks_vetorizada);
+        			desenharPontos(shape, &img, pontoDeGravidade);
+              	}
+
+
            	          
                 // You get the idea, you can get all the face part locations if
                 // you want them.  Here we just store them in shapes so we can
                 // put them on the screen.
                 shapes.push_back(shape);
             }
-
+           
+    
             // Now let's view our face poses on the screen.
             win.clear_overlay();
             win.set_image(img);
@@ -137,12 +251,13 @@ int main(int argc, char** argv)
             extract_image_chips(img, get_face_chip_details(shapes), face_chips);
             win_faces.set_image(tile_images(face_chips));
             
-  			cout << "Vetor de landmarks: ";
+  			cout << endl << "Vetor de landmarks: ";
             for(int i = 0; i < 136; i++)
             {
        	    	cout << landmarks[i] << " ";
 
             }
+            cout<< endl << "xMed: " << pontoDeGravidade.x() << endl << "yMed: " << pontoDeGravidade.y();
 
             cout << endl;
 
