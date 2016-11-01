e38626122d4eba34361e2c63095b77e085b5e1eb
Ramon Henrique <rhpr@cin.ufpe.br>
2016-11-01 07:48:45 -0300

    Adicionado no arquivo install.sh comandos para instalar opencv e dependências para o projeto

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#install required package
+apt-get install build-essential
+apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+#install opencv
+git clone https://github.com/opencv/opencv.git
+cd opencv
+mkdir release
+cd release
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local ..
+make
+sudo make install
+cd ../..
+#install dlib
 rm  -rf build
 mkdir build
 cd build
